52a8b3fc81283f2076141419dea2a5daca65f813
ccyhyeok <calvinchun@naver.com>
2023-07-18 12:44:18 +0900

    commit on 07182023

---
+++ src/main/java/com/metanet/studywithahn/StudyWithAhnApplication.java
@@ -0,0 +1,13 @@
+package com.metanet.studywithahn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWithAhnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyWithAhnApplication.class, args);
+	}
+
+}
